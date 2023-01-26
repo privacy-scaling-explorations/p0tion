a9ec1f36828ea5f36599ad6f5470c033437a6a38
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-01-26 11:42:17 +0000

    test(firebase unit): implemented unit tests for the bespoke functions related to Firebase
    Implemented unit tests for functions related to Firebase such as the ones that initialize a new app,
    retrieve the Cloud Functions and the Firestore database.
    
    fix #281

--- packages/actions/test/unit/firebase.test.ts
+++ packages/actions/test/unit/firebase.test.ts
@@ -1,7 +1,15 @@
 import { expect } from "chai"
+import { OAuthCredential } from "firebase/auth"
 import { initializeApp } from "firebase/app"
 import { fakeUsersData } from "../data/samples"
-import { getCurrentFirebaseAuthUser } from "../../src"
+import {
+    getCurrentFirebaseAuthUser,
+    getFirebaseFunctions,
+    getFirestoreDatabase,
+    initializeFirebaseApp,
+    initializeFirebaseCoreServices,
+    signInToFirebaseWithCredentials
+} from "../../src"
 import {
     createNewFirebaseUserWithEmailAndPw,
     deleteAdminApp,
@@ -17,6 +25,19 @@ import {
 describe("Firebase", () => {
     // Init admin services.
     const { adminFirestore, adminAuth } = initializeAdminServices()
+
+    // check config
+    beforeAll(() => {
+        if (
+            !process.env.FIREBASE_API_KEY ||
+            !process.env.FIREBASE_AUTH_DOMAIN ||
+            !process.env.FIREBASE_PROJECT_ID ||
+            !process.env.FIREBASE_MESSAGING_SENDER_ID ||
+            !process.env.FIREBASE_APP_ID ||
+            !process.env.FIREBASE_CF_URL_VERIFY_CONTRIBUTION
+        )
+            throw new Error("Missing environment variables for Firebase tests.")
+    })
 
     /** Authentication Core */
     describe("getCurrentFirebaseAuthUser()", () => {
@@ -72,13 +93,113 @@ describe("Firebase", () => {
         })
     })
 
-    describe("signInToFirebaseWithCredentials()", () => {})
-    describe("initializeFirebaseCoreServices()", () => {})
+    describe("signInToFirebaseWithCredentials()", () => {
+        const { userApp } = initializeUserServices()
+        it("should revert when provided the wrong credentials", async () => {
+            try {
+                await signInToFirebaseWithCredentials(userApp, new OAuthCredential())
+            } catch (error: any) {
+                expect(error.toString()).to.be.eq(
+                    "FirebaseError: Firebase: Invalid IdP response/credential: http://localhost?&providerId=undefined (auth/invalid-credential-or-provider-id)."
+                )
+            }
+        })
+        it("should revert when the application is not configured correctly", async () => {
+            expect(() => signInToFirebaseWithCredentials(initializeApp(), new OAuthCredential())).to.throw(
+                Error,
+                "Firebase: Need to provide options, when not being deployed to hosting via source. (app/no-options)."
+            )
+        })
+        it("should sign in to Firebase with the provided credentials", async () => {
+            // nb. this test requires a working OAuth2 automated flow.
+        })
+        it("should sign in to Firebase with the provided credentials and return the user", async () => {
+            // nb. this test requires a working OAuth2 automated flow.
+        })
+    })
+    describe("initializeFirebaseCoreServices()", () => {
+        it("should successfully initialize Firebase services with correct credentials", async () => {
+            const { firebaseApp, firestoreDatabase, firebaseFunctions } = await initializeFirebaseCoreServices(
+                process.env.FIREBASE_API_KEY!,
+                process.env.FIREBASE_AUTH_DOMAIN!,
+                process.env.FIREBASE_PROJECT_ID!,
+                process.env.FIREBASE_MESSAGING_SENDER_ID!,
+                process.env.FIREBASE_APP_ID!
+            )
+
+            expect(firebaseApp).to.not.be.null
+            expect(firestoreDatabase).to.not.be.null
+            expect(firebaseFunctions).to.not.be.null
+        })
+    })
 
     /** Helpers */
-    describe("initializeFirebaseApp()", () => {})
-    describe("getFirestoreDatabase()", () => {})
-    describe("getFirebaseFunctions()", () => {})
+    describe("initializeFirebaseApp()", () => {
+        it("should revert when the application is not configured correctly", async () => {
+            expect(() =>
+                initializeFirebaseApp({
+                    apiKey: "wrong",
+                    authDomain: "wrong",
+                    projectId: "wrong",
+                    messagingSenderId: "wrong",
+                    appId: "wrong"
+                })
+            ).to.throw(
+                Error,
+                "Firebase: Firebase App named '[DEFAULT]' already exists with different options or config (app/duplicate-app)."
+            )
+        })
+        it("should initialize a Firebase application", async () => {
+            const firebaseApp = initializeFirebaseApp({
+                apiKey: process.env.FIREBASE_API_KEY!,
+                authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+                projectId: process.env.FIREBASE_PROJECT_ID!,
+                messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+                appId: process.env.FIREBASE_APP_ID!
+            })
+            expect(firebaseApp).to.not.be.null
+        })
+    })
+    describe("getFirestoreDatabase()", () => {
+        it("should revert when the application is not configured correctly", async () => {
+            expect(() => getFirestoreDatabase(initializeApp())).to.throw(
+                Error,
+                "Firebase: Need to provide options, when not being deployed to hosting via source. (app/no-options)."
+            )
+        })
+        it("should return the Firestore database for a given application", async () => {
+            const firestoreDatabase = getFirestoreDatabase(
+                initializeFirebaseApp({
+                    apiKey: process.env.FIREBASE_API_KEY!,
+                    authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+                    projectId: process.env.FIREBASE_PROJECT_ID!,
+                    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+                    appId: process.env.FIREBASE_APP_ID!
+                })
+            )
+            expect(firestoreDatabase).to.not.be.null
+        })
+    })
+    describe("getFirebaseFunctions()", () => {
+        it("should revert when the application is not configured correctly", async () => {
+            expect(() => getFirebaseFunctions(initializeApp())).to.throw(
+                Error,
+                "Firebase: Need to provide options, when not being deployed to hosting via source. (app/no-options)."
+            )
+        })
+        it("should return the Firebase functions for a given application", async () => {
+            const firebaseFunctions = getFirebaseFunctions(
+                initializeFirebaseApp({
+                    apiKey: process.env.FIREBASE_API_KEY!,
+                    authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+                    projectId: process.env.FIREBASE_PROJECT_ID!,
+                    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+                    appId: process.env.FIREBASE_APP_ID!
+                })
+            )
+            expect(firebaseFunctions).to.not.be.null
+        })
+    })
 
     afterAll(async () => {
         // Delete admin app.
