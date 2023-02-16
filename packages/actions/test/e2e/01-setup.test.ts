9cece1529fd088a9278dc8f1e66f892ef599b35e
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-02-16 10:51:59 +0000

    test(setup): pr review fix
    resolved PR review comments

--- packages/actions/test/e2e/01-setup.test.ts
+++ packages/actions/test/e2e/01-setup.test.ts
@@ -102,6 +102,7 @@ describe("Setup", () => {
     // run these tests only in production mode
     if (envType === TestingEnvironment.PRODUCTION) {
         it("should revert when trying to create a ceremony with an existing prefix", async () => {
+            // @todo this test will need more work and possible refactoring of cloud functions
             // login with coordinator creds
             await signInWithEmailAndPassword(userAuth, users[1].data.email, passwords[1])
             const currentAuthenticatedCoordinator = getCurrentFirebaseAuthUser(userApp)
