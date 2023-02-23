a0ea42bb6b794ef977203ce59b0a7466a4c79f0e
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-02-23 16:24:10 +0000

    test(auth security): added test cases for the authentication security review
    Added a number of test cases part of the pentest. These test aim to ensure that the authentication
    system (Firebase and custom cloud functions) are secure against common attacks such as email
    enumeration and bruteforce attacks.

--- packages/actions/test/unit/security.test.ts
+++ packages/actions/test/unit/security.test.ts
@@ -1,21 +1,33 @@
 import chai, { assert, expect } from "chai"
 import chaiAsPromised from "chai-as-promised"
-import { getAuth, signOut, signInWithEmailAndPassword } from "firebase/auth"
+import { getAuth, signOut, signInWithEmailAndPassword, OAuthCredential, GithubAuthProvider } from "firebase/auth"
 import { where } from "firebase/firestore"
-import { fakeCeremoniesData, fakeUsersData } from "../data/samples"
+import fs from "fs"
+import path from "path"
+import { createOAuthDeviceAuth } from "@octokit/auth-oauth-device"
+import { fakeCeremoniesData, fakeCircuitsData, fakeUsersData } from "../data/samples"
 import {
-    createNewFirebaseUserWithEmailAndPw,
     deleteAdminApp,
     envType,
-    generatePseudoRandomStringOfNumbers,
     initializeAdminServices,
     initializeUserServices,
-    sleep,
-    setCustomClaims
+    generateUserPasswords,
+    createMockUser,
+    cleanUpMockUsers,
+    getAuthenticationConfiguration,
+    cleanUpMockCeremony,
+    createMockCeremony
 } from "../utils"
-import { commonTerms, generateGetObjectPreSignedUrl, getCurrentFirebaseAuthUser } from "../../src"
+import {
+    commonTerms,
+    generateGetObjectPreSignedUrl,
+    getCurrentFirebaseAuthUser,
+    isCoordinator,
+    signInToFirebaseWithCredentials
+} from "../../src"
 import { TestingEnvironment } from "../../src/types/enums"
 import { getDocumentById, queryCollection } from "../../src/helpers/database"
+import { simulateOnVerification } from "../utils/authentication"
 
 chai.use(chaiAsPromised)
 
@@ -27,42 +39,30 @@ describe("Security", () => {
     const { adminFirestore, adminAuth } = initializeAdminServices()
     const { userApp, userFunctions, userFirestore } = initializeUserServices()
     const userAuth = getAuth(userApp)
-    const user1 = fakeUsersData.fakeUser1
-    const user2 = fakeUsersData.fakeUser2
-    const user3 = fakeUsersData.fakeUser3
-    const user1Pwd = generatePseudoRandomStringOfNumbers(24)
-    const user2Pwd = generatePseudoRandomStringOfNumbers(24)
-    const user3Pwd = generatePseudoRandomStringOfNumbers(24)
+
+    const users = [fakeUsersData.fakeUser1, fakeUsersData.fakeUser2, fakeUsersData.fakeUser3]
+    const passwords = generateUserPasswords(users.length)
 
     beforeAll(async () => {
-        // create 1st user
-        await createNewFirebaseUserWithEmailAndPw(userApp, user1.data.email, user1Pwd)
-        await sleep(5000)
-
-        // Retrieve the current auth user in Firebase.
-        let currentAuthenticatedUser = getCurrentFirebaseAuthUser(userApp)
-        user1.uid = currentAuthenticatedUser.uid
-
-        // create 2nd user
-        await createNewFirebaseUserWithEmailAndPw(userApp, user2.data.email, user2Pwd)
-        await sleep(5000)
-
-        // Retrieve the current auth user in Firebase.
-        currentAuthenticatedUser = getCurrentFirebaseAuthUser(userApp)
-        user2.uid = currentAuthenticatedUser.uid
-
-        // create the coordinator
-        await createNewFirebaseUserWithEmailAndPw(userApp, user3.data.email, user3Pwd)
-        await sleep(5000)
-
-        currentAuthenticatedUser = getCurrentFirebaseAuthUser(userApp)
-        user3.uid = currentAuthenticatedUser.uid
-
-        // add coordinator privileges
-        await setCustomClaims(adminAuth, user3.uid, { coordinator: true })
+        for (let i = 0; i < users.length; i++) {
+            users[i].uid = await createMockUser(
+                userApp,
+                users[i].data.email,
+                passwords[i],
+                i === users.length - 1, // last one is coordinator
+                adminAuth
+            )
+        }
     })
 
     describe("GeneratePreSignedURL", () => {
+        beforeAll(async () => {
+            await createMockCeremony(
+                adminFirestore,
+                fakeCeremoniesData.fakeCeremonyOpenedFixed,
+                fakeCircuitsData.fakeCircuitSmallNoContributors
+            )
+        })
         it("should throw when given a bucket name that is not used for a ceremony", async () => {
             assert.isRejected(generateGetObjectPreSignedUrl(userFunctions, "nonExistent", "test"))
         })
@@ -70,14 +70,6 @@ describe("Security", () => {
         // the emulator should run without .env file thus this test would not work.
         if (envType === TestingEnvironment.PRODUCTION) {
             it("should return a pre-signed URL when given the bucket name for a ceremony", async () => {
-                // Create the mock data on Firestore.
-                await adminFirestore
-                    .collection(commonTerms.collections.ceremonies.name)
-                    .doc(fakeCeremoniesData.fakeCeremonyOpenedFixed.uid)
-                    .set({
-                        ...fakeCeremoniesData.fakeCeremonyOpenedFixed.data
-                    })
-
                 const url = await generateGetObjectPreSignedUrl(
                     userFunctions,
                     fakeCeremoniesData.fakeCeremonyOpenedFixed.data.prefix,
@@ -98,24 +90,25 @@ describe("Security", () => {
         afterAll(async () => {
             if (envType === TestingEnvironment.PRODUCTION)
                 // Delete the ceremony.
-                await adminFirestore
-                    .collection(commonTerms.collections.ceremonies.name)
-                    .doc(fakeCeremoniesData.fakeCeremonyOpenedFixed.uid)
-                    .delete()
+                await cleanUpMockCeremony(
+                    adminFirestore,
+                    fakeCeremoniesData.fakeCeremonyOpenedFixed.uid,
+                    fakeCircuitsData.fakeCircuitSmallNoContributors.uid
+                )
         })
     })
 
     describe("Security rules", () => {
         it("should allow a user to retrieve their own data from the firestore db", async () => {
             // login as user1
-            await signInWithEmailAndPassword(userAuth, user1.data.email, user1Pwd)
-            const userDoc = await getDocumentById(userFirestore, commonTerms.collections.users.name, user1.uid)
+            await signInWithEmailAndPassword(userAuth, users[0].data.email, passwords[0])
+            const userDoc = await getDocumentById(userFirestore, commonTerms.collections.users.name, users[0].uid)
             expect(userDoc.data()).to.not.be.null
         })
 
         it("should allow any authenticated user to query the ceremony collection", async () => {
             // login as user2
-            await signInWithEmailAndPassword(userAuth, user2.data.email, user2Pwd)
+            await signInWithEmailAndPassword(userAuth, users[1].data.email, passwords[1])
             // query the ceremonies collection
             expect(
                 await queryCollection(userFirestore, commonTerms.collections.ceremonies.name, [
@@ -124,17 +117,17 @@ describe("Security", () => {
             ).to.not.throw
         })
 
-        it("should throw an error if a coordiantor tries to read another user's document", async () => {
+        it("should throw an error if a coordinator tries to read another user's document", async () => {
             // login as coordinator
-            await signInWithEmailAndPassword(userAuth, user3.data.email, user3Pwd)
+            await signInWithEmailAndPassword(userAuth, users[2].data.email, passwords[2])
             // retrieve the document of another user
-            assert.isRejected(getDocumentById(userFirestore, commonTerms.collections.users.name, user1.uid))
+            assert.isRejected(getDocumentById(userFirestore, commonTerms.collections.users.name, users[0].uid))
         })
 
         it("should throw an error if an authenticated user tries to read another user's data", async () => {
             // login as user2
-            await signInWithEmailAndPassword(userAuth, user2.data.email, user2Pwd)
-            assert.isRejected(getDocumentById(userFirestore, commonTerms.collections.users.name, user1.uid))
+            await signInWithEmailAndPassword(userAuth, users[1].data.email, passwords[1])
+            assert.isRejected(getDocumentById(userFirestore, commonTerms.collections.users.name, users[0].uid))
         })
 
         afterEach(async () => {
@@ -143,16 +136,161 @@ describe("Security", () => {
         })
     })
 
+    // Tests related to authentication security
+    describe("Authentication", () => {
+        const clientType = "oauth-app"
+        const tokenType = "oauth"
+
+        // Get and assign configs.
+        const { githubClientId } = getAuthenticationConfiguration()
+        const clientId = githubClientId
+
+        let userId: string = ""
+
+        it("should not let anyone authenticate with the wrong password", async () => {
+            const wrongPassword = "wrongPassword"
+            expect(signInWithEmailAndPassword(userAuth, users[0].data.email, wrongPassword)).to.be.rejectedWith(
+                "Firebase: Error (auth/wrong-password)."
+            )
+        })
+        if (envType === TestingEnvironment.PRODUCTION) {
+            it("should prevent authentication with the wrong OAuth2 token", async () => {
+                await expect(signInToFirebaseWithCredentials(userApp, new OAuthCredential())).to.be.rejectedWith(
+                    "Firebase: Invalid IdP response/credential: http://localhost?&providerId=undefined (auth/invalid-credential-or-provider-id)."
+                )
+            })
+            // @todo might not be able to test this in code since it requires revoking access on GitHub
+            it.skip("should not be able to authenticate with a token after this is invalidated", async () => {
+                const auth = createOAuthDeviceAuth({
+                    clientType,
+                    clientId,
+                    scopes: ["gist"],
+                    onVerification: simulateOnVerification
+                })
+                const { token } = await auth({
+                    type: tokenType
+                })
+                // Get and exchange credentials.
+                const userFirebaseCredentials = GithubAuthProvider.credential(token)
+                await signInToFirebaseWithCredentials(userApp, userFirebaseCredentials)
+                const user = getCurrentFirebaseAuthUser(userApp)
+                userId = user.uid
+
+                await signOut(userAuth)
+
+                // @todo how to revoke the token programmatically?
+                await signInToFirebaseWithCredentials(userApp, userFirebaseCredentials)
+            })
+            // @todo add checks to cloud function
+            it("should prevent a user with a non reputable GitHub account from authenticating to the Firebase", async () => {})
+            it("should prevent a disabled account from loggin in (OAuth2)", async () => {
+                const auth = createOAuthDeviceAuth({
+                    clientType,
+                    clientId,
+                    scopes: ["gist"],
+                    onVerification: simulateOnVerification
+                })
+                const { token } = await auth({
+                    type: tokenType
+                })
+                // Get and exchange credentials.
+                const userFirebaseCredentials = GithubAuthProvider.credential(token)
+                await signInToFirebaseWithCredentials(userApp, userFirebaseCredentials)
+
+                const user = getCurrentFirebaseAuthUser(userApp)
+                userId = user.uid
+                // Disable user.
+                const disabledRecord = await adminAuth.updateUser(user.uid, { disabled: true })
+                expect(disabledRecord.disabled).to.be.true
+
+                await signOut(userAuth)
+
+                await expect(signInToFirebaseWithCredentials(userApp, userFirebaseCredentials)).to.be.rejectedWith(
+                    "Firebase: Error (auth/user-disabled)."
+                )
+            })
+        }
+        // @todo document this feature to prevent enumeration attacks
+        it.skip("should not allow a user to enumerate valid emails", async () => {
+            // @link https://cloud.google.com/identity-platform/docs/admin/email-enumeration-protection
+            const wrongPassword = "wrongPassword"
+            const wrongEmail = "wrongEmail"
+            expect(signInWithEmailAndPassword(userAuth, wrongEmail, wrongPassword)).to.not.be.rejectedWith(
+                "Firebase: Error (auth/invalid-email)."
+            )
+            expect(signInWithEmailAndPassword(userAuth, users[1].data.email, wrongPassword)).to.not.be.rejectedWith(
+                "Firebase: Error (auth/wrong-password)."
+            )
+        })
+        it.skip("should rate limit after a large number of failed attempts", async () => {
+            let err: any
+            try {
+                for (let i = 0; i < 1000; i++) {
+                    await expect(signInToFirebaseWithCredentials(userApp, new OAuthCredential())).to.be.rejectedWith(
+                        "Firebase: Invalid IdP response/credential: http://localhost?&providerId=undefined (auth/invalid-credential-or-provider-id)."
+                    )
+                }
+            } catch (error: any) {
+                err = error
+            }
+            expect(err).to.not.be.undefined
+        })
+        it("getCurrentFirebaseAuthUser should retun the current authenticated user and not another user's data", async () => {
+            // login as user1
+            await signInWithEmailAndPassword(userAuth, users[0].data.email, passwords[0])
+            const currentAuthenticatedUser = getCurrentFirebaseAuthUser(userApp)
+            expect(currentAuthenticatedUser.uid).to.equal(users[0].uid)
+        })
+        it("should not set a user as coordinator when they are not", async () => {
+            // login as user1
+            await signInWithEmailAndPassword(userAuth, users[0].data.email, passwords[0])
+            const currentAuthenticatedUser = getCurrentFirebaseAuthUser(userApp)
+            expect(await isCoordinator(currentAuthenticatedUser)).to.be.false
+        })
+        it("should not allow disabled accounts to login (email/password auth)", async () => {
+            // Disable user.
+            const disabledRecord = await adminAuth.updateUser(users[1].uid, { disabled: true })
+            expect(disabledRecord.disabled).to.be.true
+
+            // Try to authenticate with the disabled user.
+            await expect(signInWithEmailAndPassword(userAuth, users[1].data.email, passwords[1])).to.be.rejectedWith(
+                "Firebase: Error (auth/user-disabled)."
+            )
+
+            // re enable the user
+            const recordReset = await adminAuth.updateUser(users[1].uid, {
+                disabled: false
+            })
+            expect(recordReset.disabled).to.be.false
+        })
+        it("should lock out an account after a large number of failed attempts", async () => {
+            const passwordWordlist = fs.readFileSync(path.join(__dirname, "../data/passwords.txt"), "utf8").split("\n")
+            let err: any
+            for (const password of passwordWordlist) {
+                try {
+                    await signInWithEmailAndPassword(userAuth, users[0].data.email, password)
+                } catch (error: any) {
+                    if (error.toString() === "FirebaseError: Firebase: Error (auth/wrong-password).") {
+                        err = error.toString()
+                        break
+                    }
+                }
+            }
+            expect(err).to.be.eq(
+                "FirebaseError: Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests)."
+            )
+        })
+        afterAll(async () => {
+            // Clean OAuth user
+            await adminFirestore.collection(commonTerms.collections.users.name).doc(userId).delete()
+            await adminAuth.deleteUser(userId)
+        })
+    })
+
     // general clean up after all tests
     afterAll(async () => {
         // Clean user from DB.
-        await adminFirestore.collection("users").doc(user1.uid).delete()
-        await adminFirestore.collection("users").doc(user2.uid).delete()
-        await adminFirestore.collection("users").doc(user3.uid).delete()
-        // Remove Auth user.
-        await adminAuth.deleteUser(user1.uid)
-        await adminAuth.deleteUser(user2.uid)
-        await adminAuth.deleteUser(user3.uid)
+        await cleanUpMockUsers(adminAuth, adminFirestore, users)
         // Delete admin app.
         await deleteAdminApp()
     })
