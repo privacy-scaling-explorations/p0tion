271617d97517c59dff1f080dd37abdd3993fb1ac
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-03-22 12:23:59 +0000

    fix(ceremony verification): add extra sleep on one test case

--- packages/actions/test/e2e/02-contribution.test.ts
+++ packages/actions/test/e2e/02-contribution.test.ts
@@ -136,9 +136,11 @@ describe("Contribution", () => {
 
         // create mock ceremony with circuit data
         await createMockCeremony(adminFirestore, ceremony, tmpCircuit)
+        await sleep(1000)
     })
     if (envType === TestingEnvironment.PRODUCTION) {
         it("should allow an authenticated user to contribute to a ceremony", async () => {
+            await sleep(1000)
             // 1. login as user 2
             await signInWithEmailAndPassword(userAuth, users[2].data.email, passwords[2])
 
