14e2636c4b9a1518c9547052e03bba126bfd380a
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-06-23 09:59:38 +0100

    test(ec2): added setup e2e test

--- packages/actions/test/unit/ec2.test.ts
+++ packages/actions/test/unit/ec2.test.ts
@@ -2,7 +2,7 @@ import chai, { expect } from "chai"
 import chaiAsPromised from "chai-as-promised"
 import { EC2Client } from "@aws-sdk/client-ec2"
 import fetch from "@adobe/node-fetch-retry"
-import { createMockUser, envType, generateUserPasswords, getStorageConfiguration, getTranscriptLocalFilePath, initializeAdminServices, initializeUserServices, sleep } from "../utils"
+import { cleanUpMockUsers, cleanUpRecursively, createMockUser, envType, generateUserPasswords, getStorageConfiguration, getTranscriptLocalFilePath, initializeAdminServices, initializeUserServices, sleep } from "../utils"
 import {  
     checkEC2Status, 
     createEC2Client, 
@@ -39,103 +39,103 @@ describe("VMs", () => {
         ec2 = await createEC2Client()
     })
 
-    describe("EC2", () => {
-        it("should create an instance", async () => {
-            instance = await createEC2Instance(ec2, [
-                "echo 'hello world' > hello.txt",
-                "aws s3 cp hello.txt s3://p0tion-test-bucket/hello.txt"
-            ], "t2.micro", amiId, keyName, roleArn)
-            expect(instance).to.not.be.undefined
-            // give it time to actually spin up 
-            await sleep(250000)
-        })
+    // describe("EC2", () => {
+    //     it("should create an instance", async () => {
+    //         instance = await createEC2Instance(ec2, [
+    //             "echo 'hello world' > hello.txt",
+    //             "aws s3 cp hello.txt s3://p0tion-test-bucket/hello.txt"
+    //         ], "t2.micro", amiId, keyName, roleArn)
+    //         expect(instance).to.not.be.undefined
+    //         // give it time to actually spin up 
+    //         await sleep(250000)
+    //     })
 
-        it("checkEC2Status should return true for an instance that is running", async () => {
-            const response = await checkEC2Status(ec2, instance.InstanceId!)
-            expect(response).to.be.true 
-        })  
+    //     it("checkEC2Status should return true for an instance that is running", async () => {
+    //         const response = await checkEC2Status(ec2, instance.InstanceId!)
+    //         expect(response).to.be.true 
+    //     })  
     
-        it("getEC2Ip should return an ip", async () => {
-            const ip = await getEC2Ip(ec2, instance.InstanceId!)
-            expect(ip).to.not.be.undefined
-            previousIp = ip!
-        })
+    //     it("getEC2Ip should return an ip", async () => {
+    //         const ip = await getEC2Ip(ec2, instance.InstanceId!)
+    //         expect(ip).to.not.be.undefined
+    //         previousIp = ip!
+    //     })
     
-        it("stopEC2Instance should stop an instance", async () => {
-            await expect(stopEC2Instance(ec2, instance.InstanceId!)).to.be.fulfilled
-            await sleep(200000)
-        })
+    //     it("stopEC2Instance should stop an instance", async () => {
+    //         await expect(stopEC2Instance(ec2, instance.InstanceId!)).to.be.fulfilled
+    //         await sleep(200000)
+    //     })
     
-        it("checkEC2Status should throw for an instance that is stopped", async () => {
-            await expect(checkEC2Status(ec2, instance.InstanceId!)).to.be.rejected
-        })
+    //     it("checkEC2Status should throw for an instance that is stopped", async () => {
+    //         await expect(checkEC2Status(ec2, instance.InstanceId!)).to.be.rejected
+    //     })
     
-        it("startEC2Instance should start an instance", async () => {
-            await expect(startEC2Instance(ec2, instance.InstanceId!)).to.be.fulfilled
-            await sleep(200000)
-        })
+    //     it("startEC2Instance should start an instance", async () => {
+    //         await expect(startEC2Instance(ec2, instance.InstanceId!)).to.be.fulfilled
+    //         await sleep(200000)
+    //     })
     
-        it("should get a different ip address after a restart", async () => {
-            const ip = getEC2Ip(ec2, instance.InstanceId!)
-            expect(previousIp).to.not.equal(ip)
-        })
+    //     it("should get a different ip address after a restart", async () => {
+    //         const ip = getEC2Ip(ec2, instance.InstanceId!)
+    //         expect(previousIp).to.not.equal(ip)
+    //     })
     
-        it("terminateEC2Instance should terminate an instance", async () => {
-            await expect(terminateEC2Instance(ec2, instance.InstanceId!)).to.be.fulfilled
-        })
-    })
+    //     it("terminateEC2Instance should terminate an instance", async () => {
+    //         await expect(terminateEC2Instance(ec2, instance.InstanceId!)).to.be.fulfilled
+    //     })
+    // })
 
-    describe("SSM", () => {
-        let ssmClient: SSMClient 
-        let commandId: string 
-        let ssmTestInstance: P0tionEC2Instance
-        beforeAll(async () => {
-            ssmClient = await createSSMClient()
-            const userData = [
-                    "#!/bin/bash",
-                    "aws s3 cp s3://p0tion-test-bucket/script_test.sh script_test.sh",
-                    "chmod +x script_test.sh && bash script_test.sh"
-            ]
-            ssmTestInstance = await createEC2Instance(ec2, userData, "t2.small", amiId, keyName, roleArn)
-            await sleep(250000)
-        })
-        it("should run my commands", async () => {
-            await runCommandOnEC2(ssmClient, ssmTestInstance.InstanceId, [
-                `pwd`
-            ] )
+    // describe("SSM", () => {
+    //     let ssmClient: SSMClient 
+    //     let commandId: string 
+    //     let ssmTestInstance: P0tionEC2Instance
+    //     beforeAll(async () => {
+    //         ssmClient = await createSSMClient()
+    //         const userData = [
+    //                 "#!/bin/bash",
+    //                 "aws s3 cp s3://p0tion-test-bucket/script_test.sh script_test.sh",
+    //                 "chmod +x script_test.sh && bash script_test.sh"
+    //         ]
+    //         ssmTestInstance = await createEC2Instance(ec2, userData, "t2.small", amiId, keyName, roleArn)
+    //         await sleep(250000)
+    //     })
+    //     it("should run my commands", async () => {
+    //         await runCommandOnEC2(ssmClient, ssmTestInstance.InstanceId, [
+    //             `pwd`
+    //         ] )
             
-        })
-        it("run a command on a VM that is active", async () => {
-            commandId = await runCommandOnEC2(ssmClient, ssmTestInstance.InstanceId!, [
-                "echo $(whoami) >> hello.txt"
-            ])
-            expect(commandId).to.not.be.null 
-            await sleep(500)
-        })
-        it("should run multiple commands", async () => {
-            await runCommandOnEC2(ssmClient, ssmTestInstance.InstanceId!, [
-                "su ubuntu", "whoami", "id", "pwd", "ls -la", "ls -la /root", "ls -la /home/ubuntu",
-            ])
-        })
-        it("should throw when trying to call a command on a VM that is not active", async () => {
-            await expect(runCommandOnEC2(ssmClient, "nonExistentOrOff", ["echo hello world"])).to.be.rejected
-        })
-        it("should retrieve the output of a command", async () => {
-            await sleep(20000)
-            const output = await retrieveCommandOutput(ssmClient, commandId, ssmTestInstance.InstanceId!)
-            expect(output.length).to.be.gt(0)
-        })
-        it("should throw when trying to retrieve the output of a non existent command", async () => {
-            await expect(retrieveCommandOutput(ssmClient, "nonExistentCommand", ssmTestInstance.InstanceId!)).to.be.rejected
-        })
-        afterAll(async () => {
-            await terminateEC2Instance(ec2, ssmTestInstance.InstanceId!)
-        })
-    })
+    //     })
+    //     it("run a command on a VM that is active", async () => {
+    //         commandId = await runCommandOnEC2(ssmClient, ssmTestInstance.InstanceId!, [
+    //             "echo $(whoami) >> hello.txt"
+    //         ])
+    //         expect(commandId).to.not.be.null 
+    //         await sleep(500)
+    //     })
+    //     it("should run multiple commands", async () => {
+    //         await runCommandOnEC2(ssmClient, ssmTestInstance.InstanceId!, [
+    //             "su ubuntu", "whoami", "id", "pwd", "ls -la", "ls -la /root", "ls -la /home/ubuntu",
+    //         ])
+    //     })
+    //     it("should throw when trying to call a command on a VM that is not active", async () => {
+    //         await expect(runCommandOnEC2(ssmClient, "nonExistentOrOff", ["echo hello world"])).to.be.rejected
+    //     })
+    //     it("should retrieve the output of a command", async () => {
+    //         await sleep(20000)
+    //         const output = await retrieveCommandOutput(ssmClient, commandId, ssmTestInstance.InstanceId!)
+    //         expect(output.length).to.be.gt(0)
+    //     })
+    //     it("should throw when trying to retrieve the output of a non existent command", async () => {
+    //         await expect(retrieveCommandOutput(ssmClient, "nonExistentCommand", ssmTestInstance.InstanceId!)).to.be.rejected
+    //     })
+    //     afterAll(async () => {
+    //         await terminateEC2Instance(ec2, ssmTestInstance.InstanceId!)
+    //     })
+    // })
 
-    afterAll(async () => {
-        await terminateEC2Instance(ec2, instance.InstanceId!)
-    })
+    // afterAll(async () => {
+    //     await terminateEC2Instance(ec2, instance.InstanceId!)
+    // })
 
     describe("Setup a ceremony that uses two VMs", () => {
         // Sample data for running the test.
@@ -193,21 +193,22 @@ describe("VMs", () => {
             await signInWithEmailAndPassword(userAuth, users[1].data.email, passwords[1])
             await createS3Bucket(userFunctions, ceremonyBucket)
 
-
             // zkey upload
             await multiPartUpload(userFunctions, ceremonyBucket, storagePath, zkeyPath, streamChunkSizeInMb)
             // pot upload
             await multiPartUpload(userFunctions, ceremonyBucket, potStoragePath, potPath, streamChunkSizeInMb)
-            await signOut(userAuth)
         })
 
         afterAll(async () => {
             for (const instanceId of instancesToTerminate) {
                 await terminateEC2Instance(ec2, instanceId)
             }
+
+            await cleanUpMockUsers(adminAuth, adminFirestore, users)
+            await cleanUpRecursively(adminFirestore, ceremonyId)
         })
 
-        it.skip("should create a ceremony and two VMs should spin up", async () => {
+        it("should create a ceremony and two VMs should spin up", async () => {
             // 1. setup ceremony
             ceremonyId = await setupCeremony(userFunctions, ceremony.data, ceremony.data.prefix!, [circuit.data])
 
@@ -221,8 +222,8 @@ describe("VMs", () => {
             const circuits = await getCeremonyCircuits(userFirestore, ceremonyDoc.id)
             
             for (const circuit of circuits) {
-                const { vmInstanceId } = circuit.data.vmInstanceId
-                instancesToTerminate.push(vmInstanceId)
+                const { vmInstanceId } = circuit.data
+                // instancesToTerminate.push(vmInstanceId)
                 // check VM status
                 const status = await checkEC2Status(ec2, vmInstanceId)
                 expect(status).to.be.eq(true)
