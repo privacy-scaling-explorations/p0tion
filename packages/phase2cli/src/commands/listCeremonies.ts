d2723f2243ddfe250c086349242ccd178741978a
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-07-11 12:58:39 +0100

    feat(list ceremonies): add a command to list all ceremonies

---
+++ packages/phase2cli/src/commands/listCeremonies.ts
@@ -0,0 +1,32 @@
+import { commonTerms, getAllCollectionDocs } from "@p0tion/actions"
+import { showError } from "../lib/errors.js"
+import { bootstrapCommandExecutionAndServices } from "../lib/services.js"
+
+/**
+ * Validate ceremony setup command.
+ */
+const listCeremonies = async () => {
+    try {
+        // bootstrap command execution and services
+        const { firestoreDatabase } = await bootstrapCommandExecutionAndServices()
+
+        // get all ceremonies
+        const ceremonies = await getAllCollectionDocs(firestoreDatabase, commonTerms.collections.ceremonies.name)
+        // store all names
+        const names: string[] = []
+
+        // loop through all ceremonies
+        for (const ceremony of ceremonies) names.push(ceremony.data().prefix)
+        
+        // print them to the console
+        console.log(names.join(", "))
+        process.exit(0)
+
+    } catch (err: any) {
+        showError(`${err.toString()}`, false)
+        // we want to exit with a non-zero exit code
+        process.exit(1)
+    }
+}
+
+export default listCeremonies
