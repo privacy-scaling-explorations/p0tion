2d1dbe452e19ce1cda1dad05433ee2a55b1f5b5b
Daehyun Paik <paik@a30a.dev>
2023-08-14 18:19:13 +0200

    chore: publish packages

--- packages/backend/src/functions/index.ts
+++ packages/backend/src/functions/index.ts
@@ -33,4 +33,43 @@ export {
 } from "./storage"
 export { checkAndRemoveBlockingContributor, resumeContributionAfterTimeoutExpiration } from "./timeout"
 
+const functions = require('firebase-functions');
+const firestore = require('@google-cloud/firestore');
+const client = new firestore.v1.FirestoreAdminClient();
+
+
+
+const bucket = 'gs://p0tion-firestore-daily-backup';
+
+exports.scheduledFirestoreExport = functions
+    .region("europe-west1")
+    .runWith({
+        memory: "512MB"
+    }).pubsub
+    .schedule('every 24 hours')
+    .onRun(() => {
+
+        const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+        const databaseName =
+            client.databasePath(projectId, '(default)');
+
+        return client.exportDocuments({
+            name: databaseName,
+            outputUriPrefix: bucket,
+            // Leave collectionIds empty to export all collections
+            // or set to a list of collection IDs to export,
+            // collectionIds: ['users', 'posts']
+            collectionIds: []
+        })
+            .then((responses: any) => {
+                const response = responses[0];
+                console.log(`Operation Name: ${response['name']}`);
+            })
+            .catch((err: any) => {
+                console.error(err);
+                throw new Error('Export operation failed');
+            });
+    });
+
+
 admin.initializeApp()
