3fba30d08e4e343db0f3b2a210ac7312025d528b
NicoSerranoP <nnicosp@hotmail.com>
2024-03-05 23:35:45 -0500

    siwe(feat): using auth0 app

---
+++ packages/backend/src/functions/siwe.ts
@@ -0,0 +1,66 @@
+import dotenv from "dotenv"
+import * as functions from "firebase-functions"
+import { getAuth } from "firebase-admin/auth"
+import admin from "firebase-admin"
+import { Auth0UserInfo, CheckNonceOfSIWEAddressRequest, CheckNonceOfSIWEAddressResponse } from "../types"
+import { setEthProvider } from "../lib/services"
+
+dotenv.config()
+
+const checkNonceOfSIWEAddress = functions
+    .region("europe-west1")
+    .runWith({ memory: "512MB" })
+    .https.onCall(async (data: CheckNonceOfSIWEAddressRequest): Promise<CheckNonceOfSIWEAddressResponse> => {
+        try {
+            const { auth0Token } = data
+            const result = (await fetch(`${process.env.AUTH0_APPLICATION_URL}/userinfo`, {
+                headers: {
+                    "content-type": "application/json",
+                    authorization: `Bearer: ${auth0Token}`
+                }
+            }).then((_res) => _res.json())) as Auth0UserInfo
+            if (!result.sub) {
+                return {
+                    valid: false,
+                    message: "No user detected. Please check device flow token"
+                }
+            }
+            const auth = getAuth()
+            // check nonce
+            const address = result.nickname || result.sub
+
+            const minimumNonce = Number(process.env.ETH_MINIMUM_NONCE)
+            const nonceBlockHeight = "latest" // process.env.ETH_NONCE_BLOCK_HEIGHT
+            // look up nonce for address @block
+            let nonceOk = true
+            if (minimumNonce > 0) {
+                const provider = setEthProvider()
+                console.log(`got provider - block # ${await provider.getBlockNumber()}`)
+                const nonce = await provider.getTransactionCount(address, nonceBlockHeight)
+                console.log(`nonce ${nonce}`)
+                nonceOk = nonce >= minimumNonce
+            }
+            console.log(`checking nonce ${nonceOk}`)
+            if (!nonceOk) {
+                return {
+                    valid: false,
+                    message: "Eth address does not meet the nonce requirements"
+                }
+            }
+            await admin.auth().createUser({
+                uid: address
+            })
+            const token = await auth.createCustomToken(address)
+            return {
+                valid: true,
+                token
+            }
+        } catch (error) {
+            return {
+                valid: false,
+                message: `Something went wrong ${JSON.stringify(error)}`
+            }
+        }
+    })
+
+export default checkNonceOfSIWEAddress
---
+++ packages/phase2cli/src/commands/authSIWE.ts
@@ -0,0 +1,131 @@
+import { getAuth, signInWithCustomToken } from "firebase/auth"
+import { httpsCallable } from "firebase/functions"
+import { commonTerms } from "@p0tion/actions"
+import { showError } from "../lib/errors.js"
+import { bootstrapCommandExecutionAndServices } from "../lib/services.js"
+import theme from "../lib/theme.js"
+import { customSpinner, sleep } from "../lib/utils.js"
+import { CheckNonceOfSIWEAddressResponse, OAuthDeviceCodeResponse, OAuthTokenResponse } from "../types/index.js"
+import {
+    checkLocalAccessToken,
+    deleteLocalAccessToken,
+    getLocalAccessToken,
+    setLocalAccessToken
+} from "../lib/localConfigs.js"
+
+/**
+ * Return the token to sign in to Firebase after passing the SIWE Device Flow
+ * @param clientId <string> - The client id of the Auth0 application.
+ * @param firebaseFunctions <any> - The Firebase functions instance to call the cloud function
+ * @returns <string> - The token to sign in to Firebase
+ */
+const executeSIWEDeviceFlow = async (clientId: string, firebaseFunctions: any): Promise<string> => {
+    // Call Auth0 endpoint to request device code uri
+    const OAuthDeviceCode = (await fetch(`${process.env.AUTH0_APPLICATION_URL}/oauth/device/code`, {
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+            client_id: clientId,
+            scope: "openid",
+            audience: `${process.env.AUTH0_APPLICATION_URL}/api/v2/`
+        })
+    }).then((_res) => _res.json())) as OAuthDeviceCodeResponse
+    // TODO: poll Auth0 endpoint until you get token or request expires
+    let isSignedIn = false
+    let isExpired = false
+    let auth0Token = ""
+    while (!isSignedIn && !isExpired) {
+        // Call Auth0 endpoint to request token
+        const OAuthToken = (await fetch(`${process.env.AUTH0_APPLICATION_URL}/oauth/token`, {
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+                client_id: clientId,
+                device_code: OAuthDeviceCode.device_code,
+                grant_type: "urn:ietf:params:oauth:grant-type:device_code"
+            })
+        }).then((_res) => _res.json())) as OAuthTokenResponse
+        if (OAuthToken.error) {
+            if (OAuthToken.error === "authorization_pending") {
+                // Wait for the user to sign in
+                await sleep(OAuthDeviceCode.interval * 1000)
+            } else if (OAuthToken.error === "slow_down") {
+                // Wait for the user to sign in
+                await sleep(OAuthDeviceCode.interval * 1000 * 2)
+            } else if (OAuthToken.error === "expired_token") {
+                // The user didn't sign in on time
+                isExpired = true
+            }
+        } else {
+            // The user signed in
+            isSignedIn = true
+            auth0Token = OAuthToken.access_token
+        }
+    }
+    // TODO: send token to cloud function to check nonce, create user and retrieve token
+    const cf = httpsCallable(firebaseFunctions, commonTerms.cloudFunctionsNames.checkNonceOfSIWEAddress)
+    const result = await cf({
+        auth0Token
+    })
+    const { token, valid, message } = result.data as CheckNonceOfSIWEAddressResponse
+    if (!valid) {
+        showError(message, true)
+    }
+    return token
+}
+
+/**
+ * Auth command using Sign In With Ethereum
+ * @notice The auth command allows a user to make the association of their Ethereum account with the CLI by leveraging SIWE as an authentication mechanism.
+ * @dev Under the hood, the command handles a manual Device Flow following the guidelines in the SIWE documentation.
+ */
+const authSIWE = async () => {
+    try {
+        const { firebaseFunctions } = await bootstrapCommandExecutionAndServices()
+        // Console more context for the user.
+        console.log(
+            `${theme.symbols.info} ${theme.text.bold(
+                `You are about to authenticate on this CLI using your Ethereum address (device flow - OAuth 2.0 mechanism).\n${theme.symbols.warning} Please, note that only a Sign-in With Ethereum signature will be required`
+            )}\n`
+        )
+        const spinner = customSpinner(`Checking authentication token...`, `clock`)
+        spinner.start()
+        await sleep(5000)
+
+        // Manage OAuth Github or SIWE token.
+        const isLocalTokenStored = checkLocalAccessToken()
+
+        if (!isLocalTokenStored) {
+            spinner.fail(`No local authentication token found\n`)
+
+            // Generate a new access token using Github Device Flow (OAuth 2.0).
+            const newToken = await executeSIWEDeviceFlow(String(process.env.AUTH_SIWE_CLIENT_ID), firebaseFunctions)
+
+            // Store the new access token.
+            setLocalAccessToken(newToken)
+        } else spinner.succeed(`Local authentication token found\n`)
+
+        // Get access token from local store.
+        const token = String(getLocalAccessToken())
+
+        spinner.text = `Authenticating...`
+        spinner.start()
+
+        // Exchange token for credential.
+        const userCredentials = await signInWithCustomToken(getAuth(), token)
+        setLocalAccessToken(token)
+        spinner.succeed(`Authenticated as ${theme.text.bold(userCredentials.user.uid)}.`)
+
+        console.log(
+            `\n${theme.symbols.warning} You can always log out by running the ${theme.text.bold(
+                `phase2cli logout`
+            )} command`
+        )
+        process.exit(0)
+    } catch (error) {
+        // Delete local token.
+        console.log("An error crashed the process. Deleting local token and identity.")
+        console.error(error)
+        deleteLocalAccessToken()
+    }
+}
+
+export default authSIWE
