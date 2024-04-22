aaf54078458fff8e11b6d8f732c598750c940659
NicoSerranoP <nnicosp@hotmail.com>
2024-04-22 15:33:19 -0500

    feat: user entity

--- packages/api/src/users/entities/user.entity.ts
+++ packages/api/src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, Table, Model } from "sequelize-typescript"
+
+@Table({
+    tableName: "users"
+})
+export class User extends Model {
+    @Column
+    id: string
+
+    @Column
+    email: string
+
+    @Column
+    name: string
+
+    @Column
+    creationTime: number
+
+    @Column
+    lastSignInTime: number
+
+    @Column
+    lastUpdated: number
+}
--- packages/api/src/users/service/users.service.ts
+++ packages/api/src/users/service/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from "@nestjs/common"
 import { CreateUserDto } from "../dto/create-user.dto"
 import { UpdateUserDto } from "../dto/update-user.dto"
+import { InjectModel } from "@nestjs/sequelize"
+import { User } from "../entities/user.entity"
 
 @Injectable()
 export class UsersService {
+    constructor(
+        @InjectModel(User)
+        private userModel: typeof User
+    ) {}
+
     create(createUserDto: CreateUserDto) {
         return "This action adds a new user"
     }
