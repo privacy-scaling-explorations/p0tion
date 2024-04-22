0ee8ff86fdc60790266a0153fa95c266cadc90e8
NicoSerranoP <nnicosp@hotmail.com>
2024-04-22 13:17:12 -0500

    feat: init sqlite in nestjs

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { AuthModule } from "./auth/auth.module"
+import { SequelizeModule } from "@nestjs/sequelize"
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
+        }),
+        SequelizeModule.forRoot({
+            dialect: "sqlite",
+            storage: process.env.DB_PATH,
+            autoLoadModels: true,
+            synchronize: Boolean(process.env.DB_SYNCHRONIZE)
         }),
         AuthModule
     ]
