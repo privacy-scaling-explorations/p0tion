2b80851c065e87b45178a81b9c2672cb197c24b5
NicoSerranoP <nnicosp@hotmail.com>
2024-05-07 18:27:10 -0500

    fix: remove circuits controller and service

--- packages/api/src/ceremonies/ceremonies.module.ts
+++ packages/api/src/ceremonies/ceremonies.module.ts
@@ -4,12 +4,10 @@ import { CeremoniesService } from "./service/ceremonies.service"
 import { SequelizeModule } from "@nestjs/sequelize"
 import { CeremonyEntity } from "./entities/ceremony.entity"
 import { CircuitEntity } from "./entities/circuit.entity"
-import { CircuitsController } from "./controller/circuits.controller"
-import { CircuitsService } from "./service/circuits.service"
 
 @Module({
-    controllers: [CeremoniesController, CircuitsController],
-    providers: [CeremoniesService, CircuitsService],
+    controllers: [CeremoniesController],
+    providers: [CeremoniesService],
     imports: [SequelizeModule.forFeature([CeremonyEntity, CircuitEntity])],
     exports: [SequelizeModule]
 })
