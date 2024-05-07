cb6f747a34cf9aafeec2d1bac6742307e8c98552
NicoSerranoP <nnicosp@hotmail.com>
2024-05-07 18:30:29 -0500

    feat: create circuits function

--- packages/api/src/ceremonies/service/ceremonies.service.ts
+++ packages/api/src/ceremonies/service/ceremonies.service.ts
@@ -3,6 +3,7 @@ import { CeremonyDto } from "../dto/ceremony-dto"
 import { InjectModel } from "@nestjs/sequelize"
 import { CeremonyEntity } from "../entities/ceremony.entity"
 import { CircuitEntity } from "../entities/circuit.entity"
+import { CircuitDto } from "../dto/circuit-dto"
 
 @Injectable()
 export class CeremoniesService {
@@ -18,14 +19,19 @@ export class CeremoniesService {
 
         const ceremony = await this.ceremonyModel.create(ceremonyData as any)
 
+        const circuitEntities = await this.createCircuits(circuits, ceremony.id)
+        await ceremony.$set("circuits", circuitEntities)
+        return ceremony
+    }
+
+    async createCircuits(circuits: CircuitDto[], ceremonyId: number) {
         const circuitEntities = []
         for (let i = 0, ni = circuits.length; i < ni; i++) {
             const circuit = circuits[i]
-            const circuitEntity = await this.circuitModel.create({ ...circuit, ceremonyId: ceremony.id })
+            const circuitEntity = await this.circuitModel.create({ ...circuit, ceremonyId })
             circuitEntities.push(circuitEntity)
         }
-        await ceremony.$set("circuits", circuitEntities)
-        return ceremony
+        return circuitEntities
     }
 
     findById(id: number) {
