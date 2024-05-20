dbb8c8f8ea8afdda69ebe374d26005a66dcf9851
NicoSerranoP <nnicosp@hotmail.com>
2024-05-20 16:31:10 -0500

    feat: resume contribution after timeout expiration

--- packages/api/src/ceremonies/service/participants.service.ts
+++ packages/api/src/ceremonies/service/participants.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common"
 import { InjectModel } from "@nestjs/sequelize"
 import { ParticipantEntity } from "../entities/participant.entity"
+import { ParticipantStatus } from "@p0tion/actions"
+import { COMMON_ERRORS, SPECIFIC_ERRORS, logAndThrowError, printLog } from "src/lib/errors"
+import { LogLevel } from "src/types/enums"
 
 @Injectable()
 export class ParticipantsService {
@@ -23,5 +26,24 @@ export class ParticipantsService {
 
     findById(userId: string, ceremonyId: number) {
         return this.participantModel.findOne({ where: { userId, ceremonyId } })
+    }
+
+    async resumeContributionAfterTimeoutExpiration(ceremonyId: number, userId: string) {
+        const participant = await this.findParticipantOfCeremony(userId, ceremonyId)
+        if (!participant) {
+            logAndThrowError(COMMON_ERRORS.CM_INEXISTENT_DOCUMENT_DATA)
+        }
+        const { contributionProgress, status } = participant
+        if (status === ParticipantStatus.EXHUMED) {
+            participant.update({ status: ParticipantStatus.READY, tempContributionData: {} })
+        } else {
+            logAndThrowError(SPECIFIC_ERRORS.SE_CONTRIBUTE_CANNOT_PROGRESS_TO_NEXT_CIRCUIT)
+        }
+        printLog(
+            `Contributor ${userId} can retry the contribution for the circuit in position ${
+                contributionProgress + 1
+            } after timeout expiration`,
+            LogLevel.DEBUG
+        )
     }
 }
