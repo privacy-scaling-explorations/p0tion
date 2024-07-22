f801304e90b3667136919b02fc19efc0b365ea3f
NicoSerranoP <nnicosp@hotmail.com>
2024-07-22 13:26:27 -0500

    feat: add logout to CLI

--- packages/phase2cli/src/commands/auth/index.ts
+++ packages/phase2cli/src/commands/auth/index.ts
@@ -1,10 +1,12 @@
 import { Command } from "commander"
 import github from "./github.js"
+import logout from "../logout.js"
 
 const setAuthCommands = (program: Command) => {
     const auth = program.command("authAPI").description("manage authentication")
 
     auth.command("github").description("authenticate with Github").action(github)
+    auth.command("logout").description("logout from the current session").action(logout)
 
     return auth
 }
---
+++ packages/phase2cli/src/commands/auth/logout.ts
@@ -0,0 +1,54 @@
+import { checkAndRetrieveJWTAuth } from "src/lib-api/auth.js"
+import { showError } from "src/lib/errors.js"
+import { deleteJWTToken, deleteLocalAuthMethod } from "src/lib/localConfigs.js"
+import { askForConfirmation } from "src/lib/prompts.js"
+import theme from "src/lib/theme.js"
+import { customSpinner, sleep, terminate } from "src/lib/utils.js"
+
+const logout = async () => {
+    try {
+        const { user } = checkAndRetrieveJWTAuth()
+
+        // Inform the user about deassociation in Github and re run auth
+        console.log(
+            `${
+                theme.symbols.warning
+            } The logout could sign you out from Firebase and will delete the access token saved locally on this machine. Therefore, you have to run ${theme.text.bold(
+                "phase2cli auth"
+            )} to authenticate again.\n${
+                theme.symbols.info
+            } Remember, we cannot revoke the authorization from your Github account from this CLI! You can do this manually as reported in the official Github documentation ${
+                theme.emojis.pointDown
+            }\n\n${theme.text.bold(
+                theme.text.underlined(
+                    `https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/reviewing-your-authorized-applications-oauth`
+                )
+            )}\n`
+        )
+
+        // Ask for confirmation.
+        const { confirmation } = await askForConfirmation(
+            "Are you sure you want to log out from this machine?",
+            "Yes",
+            "No"
+        )
+
+        if (confirmation) {
+            const spinner = customSpinner(`Logging out...`, "clock")
+            spinner.start()
+
+            // Delete local token.
+            deleteLocalAuthMethod()
+            deleteJWTToken()
+
+            await sleep(3000) // ~3s.
+
+            spinner.stop()
+            console.log(`${theme.symbols.success} Logout successfully completed`)
+        } else terminate(user.displayName)
+    } catch (err: any) {
+        showError(`Something went wrong: ${err.toString()}`, true)
+    }
+}
+
+export default logout
